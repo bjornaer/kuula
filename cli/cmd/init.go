47c4bd2b68ab3744205adfc771c5a93a87e0a5df
bjornaer <maxemijo@gmail.com>
2022-03-30 23:22:28 +0100

    config file setup and init improved

--- cli/cmd/init.go
+++ cli/cmd/init.go
@@ -1,11 +1,13 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 func init() {
@@ -17,39 +19,63 @@ func init() {
 
 }
 
-func createUserConfigDir(path string) (string, error) {
-	// find and set .kuula/config.yaml config file
-	home, err := homeDir()
-	if err != nil {
-		return "", err
+func overwriteExisting(path string) bool {
+	// check if exists ~/.kuula/config.yaml config file
+	overwrite := true
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return overwrite
+	} else {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Config file found, do you wish to overwrite it? [Y/n]")
+		text, _ := reader.ReadString('\n')
+		// convert CRLF to LF
+		text = strings.Replace(text, "\n", "", -1)
+		if text == "n" {
+			overwrite = false
+		}
 	}
-	wd := filepath.Join(home, ".kuula")
-	if path != "" {
-		wd = filepath.Join(home, path)
-	}
-	err = os.Mkdir(wd, 0755)
-	if err != nil {
-		return "", err
-	}
-	return wd, nil
+	return overwrite
 }
 
-func createConfigFile() {
+func populateConfigFile() error {
 	// // prompt user for information
 	// // open pop-up to login to our page
 	// // get token or something and write to file
-	// for {
-	// 	fmt.Print("-> ")
-	// 	text, _ := reader.ReadString('\n')
-	// 	// convert CRLF to LF
-	// 	text = strings.Replace(text, "\n", "", -1)
+	confPath := viper.ConfigFileUsed()
+	fmt.Println("Using config file:", confPath)
 
-	// 	if strings.Compare("hi", text) == 0 {
-	// 		fmt.Println("hello, Yourself")
-	// 	}
+	if !overwriteExisting(confPath) {
+		fmt.Println("keeping current config")
+		return nil
+	}
 
-	// }
+	token := "<access-token>"
+	viper.Set("access-token", token)
+	viper.Set("apiVersion", "v1")
+	prompts := []string{"default project:", "username:", "favorite color:"}
 
+	reader := bufio.NewReader(os.Stdin)
+	for _, p := range prompts {
+		fmt.Print(p)
+		text, _ := reader.ReadString('\n')
+		// convert CRLF to LF
+		text = strings.Replace(text, "\n", "", -1)
+
+		if strings.Compare("default project:", p) == 0 {
+			viper.Set("default-project", text)
+			viper.Set("project", text)
+		} else if strings.Compare("username:", p) == 0 {
+			viper.Set("username", text)
+		} else if strings.Compare("favorite color:", p) == 0 {
+			fmt.Println("I like that color too!")
+		}
+
+	}
+	if err := viper.WriteConfig(); err != nil {
+		// handle failed write
+		return err
+	}
+	return nil
 }
 
 var initCmd = &cobra.Command{
@@ -58,9 +84,7 @@ var initCmd = &cobra.Command{
 	Long:  `This command can be used together with web, api or database sub-commands to init respective artifacts`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("let's setup your account")
-		customPath := args[0]
-		confDir, err := createUserConfigDir(customPath)
+		err := populateConfigFile()
 		handleError(err)
-
 	},
 }
--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -4,15 +4,24 @@ import (
 	"fmt"
 	"os"
 	"os/user"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
+// type Config struct {
+// 	ApiVersion     string `mapstructure:"apiVersion"`
+// 	Project        string `mapstructure:"project"`
+// 	CurrentProject string `mapstructure:"current-project"`
+// 	AccessToken    string `mapstructure:"access-token"`
+// 	UserName       string `mapstructure:"username"`
+// }
+
 var (
 	// This is used for config file
 	cfgFile string
-
+	// config  Config
 	rootCmd = &cobra.Command{
 		Use:   "kuula",
 		Short: "kuula: A sample CLI application",
@@ -27,7 +36,7 @@ func Execute() error {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.kuula.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.kuula/config.yaml)")
 
 }
 
@@ -48,6 +57,29 @@ func homeDir() (string, error) {
 	return user.HomeDir, nil
 }
 
+func createUserConfigDir(path string) error {
+	// check if exists and set $HOME/.kuula/ dir
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err = os.Mkdir(path, 0755)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func createUserConfigFile(path string) error {
+	// check if exists and set ~/.kuula/config.yaml config file
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		f, err := os.Create(path)
+		defer f.Close()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 /// this should contain values to connect to our service,
 // like token or svc account or whatever
 func initConfig() {
@@ -59,14 +91,36 @@ func initConfig() {
 		// Find home directory
 		home, err := homeDir()
 		handleError(err)
+		confHome := filepath.Join(home, ".kuula")
+		confName := "config"
+		confType := "yaml"
+		confPath := filepath.Join(confHome, confName+"."+confType)
+		viper.AddConfigPath(confHome)
+		viper.SetConfigName(confName)
 
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".kuula")
+		err = createUserConfigDir(confHome)
+		handleError(err)
+		err = createUserConfigFile(confPath)
+		handleError(err)
+
+		// if err := viper.SafeWriteConfig(); err != nil {
+		// 	// handle failed write
+		// 	log.Fatal("Failed writing config to file, please verify file state")
+		// }
 	}
 
 	viper.AutomaticEnv()
 
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	if err := viper.ReadInConfig(); err != nil {
+		handleError(err)
 	}
+	// log.Fatal(viper.Get("apiVersion"))
+	if viper.Get("apiVersion") == nil {
+		fmt.Println("Please run 'kuula init' to set your CLI config")
+		// os.Exit(126)
+	}
+	// viper.Unmarshal(&config)
+	// if config.ApiVersion == "" {
+	// 	fmt.Println("Please run 'kuula init' to setup your CLI")
+	// }
 }
