4e9bbeb1e317d5e24ca20736617a90c92dc5be3b
bjornaer <maxemijo@gmail.com>
2022-03-30 18:21:18 +0100

    new logo and some cli init cleanup

--- cli/cmd/init.go
+++ cli/cmd/init.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
@@ -15,16 +17,50 @@ func init() {
 
 }
 
-func createUserConfig(path string) {
-	// find and set .kuula.yaml config file
+func createUserConfigDir(path string) (string, error) {
+	// find and set .kuula/config.yaml config file
+	home, err := homeDir()
+	if err != nil {
+		return "", err
+	}
+	wd := filepath.Join(home, ".kuula")
+	if path != "" {
+		wd = filepath.Join(home, path)
+	}
+	err = os.Mkdir(wd, 0755)
+	if err != nil {
+		return "", err
+	}
+	return wd, nil
+}
+
+func createConfigFile() {
+	// // prompt user for information
+	// // open pop-up to login to our page
+	// // get token or something and write to file
+	// for {
+	// 	fmt.Print("-> ")
+	// 	text, _ := reader.ReadString('\n')
+	// 	// convert CRLF to LF
+	// 	text = strings.Replace(text, "\n", "", -1)
+
+	// 	if strings.Compare("hi", text) == 0 {
+	// 		fmt.Println("hello, Yourself")
+	// 	}
+
+	// }
+
 }
 
 var initCmd = &cobra.Command{
-	Use:     "init",
-	Aliases: []string{"dep", "depl"},
-	Short:   "init artifacts (web, api or database)",
-	Long:    `This command can be used together with web, api or database sub-commands to init respective artifacts`,
+	Use:   "init",
+	Short: "init artifacts (web, api or database)",
+	Long:  `This command can be used together with web, api or database sub-commands to init respective artifacts`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("let's setup your account")
+		customPath := args[0]
+		confDir, err := createUserConfigDir(customPath)
+		handleError(err)
+
 	},
 }
